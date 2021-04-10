f920f09e170c5f10fe94af4f6d64724678d0fbef
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2021-04-10 22:21:41 +0300

    dinamic pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
+    if (fetching) {
+      console.log("fetching");
+      axios
+        .get(
+          `https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`
+        )
+        .then((response) => {
+          setPhotos([...photos, ...response.data]);
+          setCurrentPage((prev) => prev + 1);
+        })
+        .finally(() => setFetching(false));
+    }
+  }, [fetching]);
+
+  useEffect(() => {
+    document.addEventListener("scroll", scrollHandler);
+
+    return () => {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
+  const scrollHandler = (e) => {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+      100
+    ) {
+      setFetching(true);
+    }
+
+    // console.log("scrollHeight", e.target.documentElement.scrollHeight);
+    // console.log("scrollTop", e.target.documentElement.scrollTop);
+    // console.log("innerHeight", window.innerHeight);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {photos.map((photo) => {
+        return (
+          <div key={photo.id} className="photo">
+            <div className="title">
+              {photo.id} {photo.title}
+              <img src={photo.thumbnailUrl} />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
